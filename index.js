bc13f0c39e7cc71410bf99f4f5d2537a1cf4f6f1
balijawa hussein <balijawahussein@gmail.com>
2023-10-20 12:25:15 +0300

    buttons fucntioning properly

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-$('h1').hide();
-$('p').hide();
 $(document).ready(function() {
+    $('h1').hide();
+    $('p').hide();
     $('h1').slideDown(5000).fadeIn(5000);
-});
-$(document).ready(function() {
     $('p').fadeIn(9000);
 });
+
 function transy() {
     $('.nav-links').css({
-        "transform": "translatey(0)",
+        "transform": "translateY(0)",
         "z-index": "10",
         "color": "white"
     });
 }
+
 $('.burger').click(transy);
-function hide(){
-    $('.nav-links').css("transform","translatey(-100%)")
+
+function hide() {
+    $('.nav-links').css("transform", "translateY(-100%)");
 }
+
 $('.burger-1').click(hide);
-// buttons fnc
-// add class active
 
 const nextProject = () => {
-    const gallery = document.querySelector('.gallery');
-    const activeClass = document.querySelector('.active');
-    
-    if (activeClass) {
-        activeClass.classList.remove('active');
-        if (activeClass.nextElementSibling) {
-            activeClass.nextElementSibling.classList.add('active'); 
+    const gallery = $('.gallery');
+    const activeClass = $('.active');
+
+    if (activeClass.length > 0) {
+        activeClass.removeClass('active');
+        if (activeClass.next().length > 0) {
+            activeClass.next().addClass('active');
         } else {
-            gallery.children[0].classList.add('active');
-        }
-    }
-}
-const prevProject = () => {
-    const gallery = document.querySelector('.gallery');
-    const activeClass = document.querySelector('.active');
-    
-    if (activeClass) {
-        activeClass.classList.remove('active');
-        if (activeClass.nextElementSibling) {
-            activeClass.nextElementSibling.classList.add('active'); 
-        } else {
-            gallery.children[0].classList.add('active');
+            gallery.children().first().addClass('active');
         }
     }
 }
 
-const btn1 = document.querySelector('.btn1').addEventListener('click', prevProject);
-const btn2 = document.querySelector('.btn2').addEventListener('click', nextProject);
+const prevProject = () => {
+    const gallery = $('.gallery');
+    const activeClass = $('.active');
+
+    if (activeClass.length > 0) {
+        activeClass.removeClass('active');
+        if (activeClass.prev().length > 0) {
+            activeClass.prev().addClass('active');
+        } else {
+            gallery.children().last().addClass('active');
+        }
+    }
+}
+
+$('.btn1').click(prevProject);
+$('.btn2').click(nextProject);
