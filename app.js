55b7221e7f44c4d7e6b2389a7d20237c939ed0f7
balijawa hussein <balijawahussein@gmail.com>
2024-01-24 17:50:54 +0300

    creeating routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
@@ -8,8 +8,8 @@ app.use(express.static('public'));
 app.get ('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 });
-app.get ('/pages/why-us', (req, res)=>{
-    res.sendFile(__dirname + 'pages/why-us.html')
+app.get ('/why-us', (req, res)=>{
+    res.sendFile(__dirname + '/why-us.html')
 });
 app.listen(port,()=>{
     console.log('server running on port ${port}')
