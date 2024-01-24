7e239612399269030d2a3ce54a5b1c3450069897
balijawa hussein <balijawahussein@gmail.com>
2024-01-24 13:39:48 +0300

    setting up routes

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(express.static('public'));
 app.get ('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 });
-
+app.get ('/pages/why-us', (req, res)=>{
+    res.sendFile(__dirname + 'pages/why-us.html')
+});
 app.listen(port,()=>{
     console.log('server running on port ${port}')
 });
