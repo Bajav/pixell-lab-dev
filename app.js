471c8f498d9005643692af1abe05c37628021ad1
balijawa hussein <balijawahussein@gmail.com>
2024-01-24 20:23:54 +0300

    making the routes work

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.get ('/', (req, res)=>{
 app.get ('/why-us', (req, res)=>{
     res.sendFile(__dirname + '/why-us.html')
 });
+app.get ('/services', (req, res)=>{
+    res.sendFile(__dirname + '/services.html')
+});
+app.get ('/past-projects', (req, res)=>{
+    res.sendFile(__dirname + '/past-projects.html')
+});
+app.get ('/pricing', (req, res)=>{
+    res.sendFile(__dirname + '/pricing.html')
+});
+app.get ('/contact', (req, res)=>{
+    res.sendFile(__dirname + '/contact.html')
+});
+app.get ('/index', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+});
 app.listen(port,()=>{
     console.log('server running on port ${port}')
 });
